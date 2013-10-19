1ca8b113cc6032a3f97267d383f8bec6be38cafd
lcurty <lcurty@gmail.com>
2013-10-19 13:43:36 -0500

    Additions and edits
    Update to users control, master template, index, and login pages. Create
    styles.css and set background, create users profile. Add login/logout
    functionality.

--- views/_v_template.php
+++ views/_v_template.php
@@ -4,15 +4,40 @@
 	<title><?php if(isset($title)) echo $title; ?></title>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
-					
+
+	<!-- Global JS/CSS -->
+	<link href="../css/styles.css" rel="stylesheet" type="text/css" media="all">				
+
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
 	
 </head>
 
-<body>	
+<body>  
 
-	<?php if(isset($content)) echo $content; ?>
+  <div id='menu'>
+
+      <a href='/'>Home</a>
+
+      <!-- Menu for users who are logged in -->
+      <?php if($user): ?>
+
+          <a href='/users/logout'>Logout</a>
+          <a href='/users/profile'>Profile</a>
+
+      <!-- Menu options for users who are not logged in -->
+      <?php else: ?>
+
+          <a href='/users/signup'>Sign up</a>
+          <a href='/users/login'>Log in</a>
+
+      <?php endif; ?>
+
+  </div>
+
+  <br>
+
+  <?php if(isset($content)) echo $content; ?>
 
 	<?php if(isset($client_files_body)) echo $client_files_body; ?>
 </body>
--- views/v_users_login.php
+++ views/v_users_login.php
@@ -2,13 +2,13 @@
   <fieldset>
   	<legend>Login</legend>
     <p>
-      <label for="username" id="username_label">Username</label><br />
-      <input type="text" name="username" id="username" size="38" value="" class="text-input" />
+      <label for="username" id="email_label">Email</label><br />
+      <input type="text" name="email" id="email" size="38" value="" class="text-input" />
     </p>
     <p>
       <label for="password" id="password_label">Password</label><br />
       <input type="password" name="password" id="password" size="38" value="" class="text-input" />
     </p>
-    <p class="center"><input type="submit" class="button" id="submit_btn" value="Sign Up" /></p>
+    <p class="center"><input type="submit" class="button" id="submit_btn" value="Login" /></p>
   </fieldset>  
 </form>
