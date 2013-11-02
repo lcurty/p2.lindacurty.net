cbec1067e196a70a27b2e3bbffa27c1073828f79
lcurty <lcurty@gmail.com>
2013-11-02 10:59:31 -0500

    building comment boxes

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -33,7 +33,9 @@
 								ON posts.user_id = users_users.user_id_followed
 						INNER JOIN users 
 								ON posts.user_id = users.user_id
-						WHERE users_users.user_id = '.$this->user->user_id.' OR posts.user_id = '.$this->user->user_id;
+						WHERE users_users.user_id = '.$this->user->user_id.' OR posts.user_id = '.$this->user->user_id.'
+						ORDER BY posts.created DESC';
+						
 		
 				# Run posts query, store the results in the variable $posts
 				$posts = DB::instance(DB_NAME)->select_rows($q);
@@ -51,10 +53,21 @@
 		
 				# Run the query, store the results in the variable $comments
 				$comments = DB::instance(DB_NAME)->select_rows($q);
+		
+				# Count of comments per post - used for styling
+				$q = 'SELECT post_id, comment_id, count( post_id )
+							FROM comments
+							GROUP BY post_id';
+		
+				# Run the query, store the results in the variable $comment_count
+				$comment_count = DB::instance(DB_NAME)->select_rows($q);
+				
+				
 
 				# Pass data to the View
 				$this->template->content->posts = $posts;
 				$this->template->content->comments = $comments;
+				$this->template->content->comment_count = $comment_count;
 		
 				# Render the View
 				echo $this->template;
--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -1,10 +1,13 @@
 <!-- Display Post Field -->
 <?php include 'v_posts_add.php' ?>
 
+<hr class="clear" />
+
 <!-- Loop through posts for user and all follows -->
 <?php foreach($posts as $post): ?>
 
-  <article>
+
+  <article class="posts<?php foreach($comment_count AS $this_count): ?><?php if($this_count['post_id'] == $post['post_id']): ?> has_comment<?php endif ?><?php endforeach; ?>">
   
       <p class="posted_by"><?=$post['first_name']?> <?=$post['last_name']?></p>
   
@@ -21,27 +24,37 @@
       </div>
     
   </article>
+ 
 
-	<!-- Loop through comments for each post -->
+
+  <div class="comment_box">
+  
+		<!-- Loop through comments for each post -->
   
     <?php foreach($comments AS $comment): ?>
-    	
-			<?php if($comment['post_id'] == $post['post_id']): ?>
+      
+      <?php if($comment['post_id'] == $post['post_id']): ?>
                   
-      	<article>
+        <article class="comments">
   
           <div class="post_comment">
     
-            <p class="comment_by"><?=$comment['first_name']?> <?=$comment['last_name']?></p>
-            <p><?=$comment['comment']?></p>
+            <p><span class="comment_by"><?=$comment['first_name']?> <?=$comment['last_name']?></span> &ndash; <?=$comment['comment']?></p>
+
+            <time datetime="<?=Time::display($comment['created'],'Y-m-d G:i')?>">
+                <?=Time::display($comment['created'])?>
+            </time>
     
           </div>
         
         </article>
-
+  
       <?php endif; ?>
-		
-		<?php endforeach; ?>
- 
+    
+    <?php endforeach; ?>
+  
+  </div>
+  
+  <hr class="clear" />
 
 <?php endforeach; ?>
